e639fc2974666942a6fdbdd90d48ed800aca2a41
Sara Baqla <missatrox44@gmail.com>
2022-11-06 09:09:10 -0600

    ability to add new task

--- broken-react-todo-application-context-api-master 2/src/components/todo-form/todo-form.jsx
+++ broken-react-todo-application-context-api-master 2/src/components/todo-form/todo-form.jsx
@@ -1,14 +1,30 @@
 import * as React from 'react';
 import { TodosContext } from '../../todo-context';
 import './todo-form.scss';
+import { v4 } from 'uuid';
 
 export const TodoForm = () => {
   const { todos, setTodos } = React.useContext(TodosContext);
   const [task, setTask] = React.useState('');
 
-  const handleAddTodo = () => {
-    // Fin an ability to add new task
-  };
+ //handleAddToDo
+ const handleAddTodo = () => {
+  console.log('Add task clicked');
+  console.log('Text input value:', task);
+  // console.log(typeof task);
+  // e.preventDefault();
+
+  //add task to todoList
+  setTodos([...todos,
+  {
+    id: v4(),
+    label: task,
+    checked: false
+  }])
+
+  //clears out input after task added
+  setTask('');
+};
 
   const handleKeyUp = (e) => {
     if (e.keyCode === 13) {
